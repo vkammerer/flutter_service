9b24f656f727fa250934fff1679795d8aec5ccae
Vincent <vkammerer@gmail.com>
2020-04-29 12:46:28 +1200

    Fixes channel name on Dart side

--- lib/flutter_service.dart
+++ lib/flutter_service.dart
@@ -3,8 +3,9 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterService {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_service');
+  static const String _channelName =
+      "com.vincentkammerer.flutter_service/channel";
+  static const MethodChannel _channel = const MethodChannel(_channelName);
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
