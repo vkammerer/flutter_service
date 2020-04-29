b7bf749203f0f8612cffcd4e9dbf3d524a20037c
Vincent <vkammerer@gmail.com>
2020-04-29 12:43:26 +1200

    Removes unnecessary comments and cleans base Java file

--- android/src/main/java/com/vincentkammerer/flutter_service/FlutterServicePlugin.java
+++ android/src/main/java/com/vincentkammerer/flutter_service/FlutterServicePlugin.java
@@ -1,7 +1,6 @@
 package com.vincentkammerer.flutter_service;
 
 import androidx.annotation.NonNull;
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -9,32 +8,30 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FlutterServicePlugin */
+/**
+ * FlutterServicePlugin
+ */
 public class FlutterServicePlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+
+  private static final String CHANNEL_NAME = "com.vincentkammerer.flutter_service/channel";
   private MethodChannel channel;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutter_service");
-    channel.setMethodCallHandler(this);
+    // TODO: Move channel instantiation to initialize method using flutterBinding.getBinaryMessenger ?
+    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(),
+        CHANNEL_NAME);
+    initizalize();
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_service");
-    channel.setMethodCallHandler(new FlutterServicePlugin());
+    FlutterServicePlugin instance = new FlutterServicePlugin();
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_NAME);
+    instance.initizalize();
+  }
+
+  private void initizalize() {
+    channel.setMethodCallHandler(this);
   }
 
   @Override
